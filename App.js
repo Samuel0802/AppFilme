1a6c28ec4d30c99b1d58bbc88cc8025b36e23dc9
zyz <samuelsouza0802@gmail.com>
2023-05-12 23:54:16 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+    FlatList,
+    StyleSheet,
+    Text,
+    ActivityIndicator
+  
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import api from './src/services/api';
+import Filmes from './src/Filmes';
+
+
+class App extends Component {
+
+constructor(props){
+  super(props);
+
+  this.state ={
+   filmes: [],
+   loading: true
+  };
+};
+
+
+  //componentDidMount - quando o componente é montado em tela
+ async componentDidMount(){
+   const response = await api.get('r-api/?api=filmes'); //Buscar informação da api
+ 
+    this.setState({
+      filmes: response.data,
+      loading: false
+    });
+  }
+
+  render() {
+
+    if(this.state.loading){
+         return( //Seria a bolinha que carregava
+          <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+             <ActivityIndicator color='#09a6ff' size={40}/>
+          </View>
+         );
+    }else{
+      return (
+        <View style={styles.container}>
+      
+        <FlatList
+         data={this.state.filmes}
+         keyExtractor={item => item.id.toString()}
+         renderItem={({item}) => <Filmes data={item} />}
+        />
+          
+        </View>
+      );
+    }
+ 
+   
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: 25
+       
+  }
+  
 });
+
+
+export default App;
